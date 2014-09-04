d7c12e986840d508c30af58773fd1632bb9111eb
ipinyol <ipinyol@gmail.com>
2014-09-04 09:43:45 +0200

    Adding comments in class Constants in end points

--- src/main/java/com/imath/connect/util/Constants.java
+++ src/main/java/com/imath/connect/util/Constants.java
@@ -7,13 +7,28 @@ public class Constants {
     static public final String baseURL = "/api/" + VERSION;
     
     // The EndPonts URLS of the API
+    // Adds a new project
     static public final String newProject = "/newProject";
+    
+    // Gets the instances, both public and private (given the user)
     static public final String instances = "/instances";
+    
+    // Gets the own projects given the user
     static public final String ownProjects = "/ownProjects";
+    
+    // gets the collaborative projects given a user
     static public final String colProjects = "/colProjects";
+    
+    // get the standard configurations
     static public final String configurations = "/configurations";
+    
+    // gets the user information given the username
     static public final String getUserByUserName = "/getUserByUserName";
+    
+    // gets the user information given the uuid of the user
     static public final String getUser = "/getUser";
+    
+    // gets the users that collaborate on a project, given the project
     static public final String getColUsersByProjectUser = "/getColUsersByProject";
     
 }
