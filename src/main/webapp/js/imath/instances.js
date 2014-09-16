26ad6db3b6bfa3957ffc66c6a15753f9cb69feca
ipinyol <ipinyol@imathresearch.com>
2014-09-16 15:04:23 +0200

    #26 Task done

--- src/main/webapp/js/imath/instances.js
+++ src/main/webapp/js/imath/instances.js
@@ -1,10 +1,12 @@
 
 function placeLayoutInstances(uuid_instance) {
+	// Not active in the current version
+	/*
 	setSelectMenu("imath-id-instances-menu");
 	jQuery.get('instances.html', function(data) {
 		$(".imath-main-row").html(data);
 		initInstanceView(uuid_instance);
-	});
+	})*/;
 }
 
 function initInstanceView(uuid) {
