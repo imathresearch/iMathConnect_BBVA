8fc39a7bd26ea3b487b74067d5b67e22f9f112b0
ipinyol <ipinyol@imathresearch.com>
2014-09-22 11:08:49 +0200

    #40 IMathCloudAccess adaptation

--- src/main/java/com/imath/connect/util/IMathCloudAccess.java
+++ src/main/java/com/imath/connect/util/IMathCloudAccess.java
@@ -8,31 +8,17 @@ import com.util.AuthenticUser;
 @Stateless
 public class IMathCloudAccess implements IMathCloudInterface{
 	
-	// Important to be synchronized!!!
-	// Since iMathCloud.setBaseURL is static, only one instance of such base is possible and thus,
-	// if two threads access the method, some conflicts may appear.
-    
-    private Object lock = new Object();     // A mutex to sync
+    // Sync is produced at the iMathCloud object level
     
     @Override
     public synchronized void newProject(String privateProjectName, String keyAccess, String publicProjectName, String baseURL) throws Exception {
-        
-        synchronized(lock) {
-            iMathCloud.setBaseURL(baseURL);
-            iMathCloud.registerUser(privateProjectName, keyAccess, publicProjectName, "");
-            iMathCloud.setBaseURL(null);
-        }
+        iMathCloud.registerUserSync(publicProjectName, keyAccess, publicProjectName, "", baseURL);
+    
 	}
 	
     @Override
     public synchronized void removeProject(String privateProjectName, String key, String baseURL) throws Exception {
-        synchronized(lock) {
-            iMathCloud.setBaseURL(baseURL);
-            AuthenticUser authUser = new AuthenticUser(privateProjectName, key);
-            iMathCloud.removeUser(authUser);
-            iMathCloud.setBaseURL(null);
-        }
-	}
-    
-    
+        AuthenticUser authUser = new AuthenticUser(privateProjectName, key);
+        iMathCloud.removeUserSync(authUser, baseURL);
+    }
 }
